a76cabc766288ceffc7bc097b44989d513543ec5
ewn <83999562+ewn69@users.noreply.github.com>
2023-02-16 13:02:23 +0800

    Update create.sh

--- create.sh
+++ create.sh
@@ -26,60 +26,53 @@ show_free_memory() {
 
 # Function to show free disk space
 show_free_disk_space() {
-  echo "=========================================="
   echo "Free Disk Space:"
-  df -h | awk '$NF=="/"{printf "%s", $4}'
-  echo ""
+  df -h /
   echo "=========================================="
 }
 
 # Ask user for swap size
-echo "How many GB of swap space do you want to create? [ ex: 8 ]"
+echo -n "How many GB of swap space do you want to create? [ ex: 8 ] | Input 1-128: "
 read swap_size
 
-# Validate input swap size
-if ! [[ "$swap_size" =~ ^[0-9]+$ ]]; then
-  echo "Invalid input. Please enter a valid integer."
-  exit 1
-fi
-
-if [ "$swap_size" -gt 128 ]; then
-  echo "Swap size cannot exceed 128GB."
-  exit 1
-fi
-
-# Check disk space availability
-if ! df / | awk '{print $4}' | tail -1 | awk '{ if ($1 < '$swap_size'000000) exit 1 }'; then
-  echo "Not enough disk space available for swap creation."
+# Check that input is within range of 1-128
+if ! [[ "$swap_size" =~ ^[1-9]$|^[1-9][0-9]$|^1[0-2][0-8]$ ]]; then
+  echo "Invalid input. Please enter a value between 1-128."
   exit 1
 fi
 
 # Confirm swap creation
-echo "Do you want to create a $swap_size GB swap file? [Y/N]"
+echo -n "Do you want to create a $swap_size GB swap file? [Y/N]: "
 read confirmation
 
 if [ "$confirmation" = "Y" ] || [ "$confirmation" = "y" ]; then
+  # Check if there is enough free disk space
+  if [ $(df -BM / | awk '{print $4}' | tail -1 | sed 's/M//') -lt $swap_size ]; then
+    echo "Not enough free disk space. Aborting."
+    exit 1
+  fi
+
   # Create swap file
   if ! fallocate -l "$swap_size"G /swapfile &> /dev/null; then
     echo "Failed to create swap file."
     exit 1
   fi
-  
+
   # Set permissions and format swap file
   chmod 600 /swapfile &> /dev/null
   if ! mkswap /swapfile &> /dev/null; then
     echo "Failed to format swap file."
     exit 1
   fi
-  
+
   # Enable swap file and set to mount on boot
   if ! swapon /swapfile &> /dev/null; then
     echo "Failed to enable swap file."
     exit 1
   fi
   echo "/swapfile swap swap defaults 0 0" | tee -a /etc/fstab &> /dev/null
-  
-  # Show free memory and free disk space
+
+  # Show free memory and disk space
   show_free_memory
   show_free_disk_space
   
